efa532b392b1ac836f010fa8f611465f3ee5fcbc
PoojaSwami3010 <63848720+PoojaSwami3010@users.noreply.github.com>
2023-09-28 14:22:12 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import './style.css';
+import { Test } from './Test.js';
 
 export default function App() {
-  // const [brand, setBrand] = React.useState("Ford");
-  // const [model, setModel] =React. useState("Mustang");
-  // const [year, setYear] =React. useState("1964");
-  // const [color, setColor] =React. useState("red");
-  const [car, setCar] = React.useState({
-    brand: 'Ford',
-    model: 'Mustang',
-    year: '1964',
-    color: 'red',
-  });
-
   return (
     <>
-      <h1>My {car.brand}</h1>
-      <p>
-        {/* It is a {color} {model} from {year}. */}
-        It is a {car.color} {car.model} from {car.year}.
-      </p>
+      <Test />
     </>
   );
 }
---
+++ src/Test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export const Test = () => {
+  // const [brand, setBrand] = React.useState("Ford");
+  // const [model, setModel] =React. useState("Mustang");
+  // const [year, setYear] =React. useState("1964");
+  // const [color, setColor] =React. useState("red");
+  const [car, setCar] = React.useState({
+    brand: 'Ford',
+    model: 'Mustang',
+    year: '1964',
+    color: 'red',
+  });
+  return (
+    <div>
+      <h1>My {car.brand}</h1>
+      <p>
+        {/* It is a {color} {model} from {year}.  */}
+        It is a {car.color} {car.model} from {car.year}.
+      </p>
+    </div>
+  );
+};
